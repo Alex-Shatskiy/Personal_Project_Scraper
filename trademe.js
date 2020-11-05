3666e59fd522830ca01b660e6734967361e9359d
student <student@eda-bc-07.localdomain>
2020-11-05 15:15:46 +1300

    write to file

--- trademe.js
+++ trademe.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs');
 
 
 async function scraper(){
     try{
-        let url = 'https://www.trademe.co.nz/a/property/residential/rent/wellington/wellington/search?bedrooms_min=1&bathrooms_min=2&sort_order=expirydesc&bedrooms_max=2'
+        let url = 'https://www.trademe.co.nz/a/property/residential/rent/wellington/wellington/search?bedrooms_min=1&sort_order=expirydesc&bedrooms_max=2'
         const browser = await puppeteer.launch({headless: true})
         const page = await browser.newPage()
         page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36")
@@ -12,17 +13,39 @@ async function scraper(){
             timeout :0
         })
 
-        const homes = await page.evaluate( () => Array.from( document.querySelectorAll( 'tm-property-search-card-listing-title' ), element => element.textContent));
-        const avalibleDates = await page.evaluate( () => Array.from( document.querySelectorAll( 'tm-property-search-card-address-subtitle' ), element => element.textContent.match(/([^-]*)Dec/g)));
-        const price = await page.evaluate( () => Array.from( document.querySelectorAll( '.tm-property-search-card-price-attribute__price' ), element => element.textContent.replace(/[^0-9\.]/g,'')));
-     
+        const homes = await page.evaluate( 
+            () => Array.from( 
+                document.querySelectorAll( 'tm-property-search-card-listing-title' ), 
+                element => element.textContent
+              )
+            );
         
+        const avalibleDates = await page.evaluate( 
+            () => Array.from( 
+                document.querySelectorAll( 'tm-property-search-card-address-subtitle'), 
+                element => element.textContent.match(/([^-]*)Dec/g)
+              )
+            );
+
+        const price = await page.evaluate( 
+            () => Array.from( 
+                document.querySelectorAll( '.tm-property-search-card-price-attribute__price' ), 
+                element => element.textContent.replace(/[^0-9\.]/g,'')
+              )
+            );
         
+        const links = await page.evaluate(
+            () => Array.from(
+              document.querySelectorAll('.tm-property-search-card__link'),
+              a => a.getAttribute('href')
+            )
+          );
+
         
         let allHomes = []
         for(let i=0; i< homes.length;i++){
-         if(avalibleDates[i] != null && price[i] < 1000){   
-             allHomes.push({id: i, homes: homes[i], dates: avalibleDates[i], price: price[i]})
+            if(avalibleDates[i] != null && price[i] < 1000){   
+                allHomes.push({id: i, homes: homes[i], dates: avalibleDates[i][0], price: price[i], links: 'trademe.co.nz/a/'+links[i]})
          }
         }
         // 
@@ -30,8 +53,11 @@ async function scraper(){
         //     waitUntil: 'load',
         //     timeout :0
         // })
-        console.log(allHomes)
-        return allHomes
+        // console.log(allHomes)
+        await fs.writeFile('file.json', JSON.stringify(allHomes,null,2),'utf8', (e) => {
+            if (e) throw e;
+            else console.log('File Writen')
+          });
     }catch(e){
         console.log('you got this error' + e)
  }
